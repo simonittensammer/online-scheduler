fcaa39f8166c71ae3d6ff968685bf8a9098eb3b7
simonittensammer <simon.ittensammer@gmx.at>
2021-01-03 19:42:12 +0100

    add attributes to Appointment class

--- scheduler-backend/src/main/java/at/htl/entity/Appointment.java
+++ scheduler-backend/src/main/java/at/htl/entity/Appointment.java
@@ -1,4 +1,23 @@
 package at.htl.entity;
 
-public class Appointment {
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+public class Appointment extends PanacheEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String title;
+
+    String desc;
+
+    LocalDateTime start;
+
+    LocalDateTime end;
 }
