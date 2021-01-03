95422a02a09c9f398369946e7f70b0bc28fcd287
simonittensammer <simon.ittensammer@gmx.at>
2021-01-03 19:40:20 +0100

    add attributes to Calendar class

--- scheduler-backend/src/main/java/at/htl/entity/Calendar.java
+++ scheduler-backend/src/main/java/at/htl/entity/Calendar.java
@@ -1,4 +1,15 @@
 package at.htl.entity;
 
-public class Calendar {
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Id;
+
+public class Calendar extends PanacheEntity {
+
+    @Id
+    String name;
+
+    String desc;
+
+    String pw;
 }
