84020bbc2d7448b4c50a9f612837487d5b37f7a1
simonittensammer <simon.ittensammer@gmx.at>
2021-01-04 21:07:29 +0100

    add annotations to repositories

--- scheduler-backend/src/main/java/at/htl/control/AppointmentRepository.java
+++ scheduler-backend/src/main/java/at/htl/control/AppointmentRepository.java
@@ -3,5 +3,10 @@ package at.htl.control;
 import at.htl.entity.Appointment;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@Transactional
+@ApplicationScoped
 public class AppointmentRepository implements PanacheRepository<Appointment> {
 }
--- scheduler-backend/src/main/java/at/htl/control/CalendarRepository.java
+++ scheduler-backend/src/main/java/at/htl/control/CalendarRepository.java
@@ -3,5 +3,10 @@ package at.htl.control;
 import at.htl.entity.Calendar;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+
+@Transactional
+@ApplicationScoped
 public class CalendarRepository implements PanacheRepository<Calendar> {
 }
