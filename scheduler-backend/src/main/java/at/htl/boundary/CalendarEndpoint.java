d9c44ae055635c8015e28b5b8e6afc274156c22f
simonittensammer <simon.ittensammer@gmx.at>
2021-01-04 21:22:15 +0100

    inject CalendarRepository in CalendarEndpoint

--- scheduler-backend/src/main/java/at/htl/boundary/CalendarEndpoint.java
+++ scheduler-backend/src/main/java/at/htl/boundary/CalendarEndpoint.java
@@ -1,4 +1,11 @@
 package at.htl.boundary;
 
+import at.htl.control.CalendarRepository;
+
+import javax.inject.Inject;
+
 public class CalendarEndpoint {
+
+    @Inject
+    CalendarRepository cr;
 }
