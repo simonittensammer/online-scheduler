260ebe6f8409d0a5a9437d38a38ccedd76c5ba39
simonittensammer <simon.ittensammer@gmx.at>
2021-01-04 21:22:48 +0100

    inject AppointmentRepository in AppointmentEndpoint

--- scheduler-backend/src/main/java/at/htl/boundary/AppointmentEndpoint.java
+++ scheduler-backend/src/main/java/at/htl/boundary/AppointmentEndpoint.java
@@ -1,4 +1,11 @@
 package at.htl.boundary;
 
+import at.htl.control.AppointmentRepository;
+
+import javax.inject.Inject;
+
 public class AppointmentEndpoint {
+
+    @Inject
+    AppointmentRepository ar;
 }
