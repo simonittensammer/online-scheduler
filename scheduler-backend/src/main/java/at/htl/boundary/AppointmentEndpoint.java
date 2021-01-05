508b666390a4778c5146cb300c042fb7181f3b7b
simonittensammer <simon.ittensammer@gmx.at>
2021-01-05 18:03:08 +0100

    add post appointment

--- scheduler-backend/src/main/java/at/htl/boundary/AppointmentEndpoint.java
+++ scheduler-backend/src/main/java/at/htl/boundary/AppointmentEndpoint.java
@@ -1,11 +1,34 @@
 package at.htl.boundary;
 
 import at.htl.control.AppointmentRepository;
+import at.htl.entity.Appointment;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
+@Path("/appointment")
+@ApplicationScoped
+@Transactional
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class AppointmentEndpoint {
 
     @Inject
     AppointmentRepository ar;
+
+    @GET
+    public List<Appointment> getAll() {
+        return ar.listAll();
+    }
+
+    @POST
+    public Response addAppointment(Appointment appointment) {
+        ar.persist(appointment);
+        return Response.ok(appointment).build();
+    }
 }
