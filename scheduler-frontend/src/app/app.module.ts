94a3ac5986d0826b0e082a0af9d39ba2fb034bc7
simonittensammer <simon.ittensammer@gmx.at>
2021-01-06 00:37:45 +0100

    create calendar-list component

--- scheduler-frontend/src/app/app.module.ts
+++ scheduler-frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
+import { CalendarListComponent } from './calendar-list/calendar-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarListComponent
   ],
   imports: [
     BrowserModule,
