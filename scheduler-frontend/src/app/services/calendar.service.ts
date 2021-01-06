2714aece5ef32099b34d74cacb910baa7f050ff3
simonittensammer <simon.ittensammer@gmx.at>
2021-01-06 01:36:00 +0100

    create getAllCalendars in calendar.service

--- scheduler-frontend/src/app/services/calendar.service.ts
+++ scheduler-frontend/src/app/services/calendar.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Calendar} from '../models/calendar';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
+  SERVER_URL = 'http://localhost:8080/';
 
-  constructor() { }
+  calendarList: Array<Calendar> = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllCalendars(): Observable<Array<Calendar>> {
+    return this.http.get<Array<Calendar>>(this.SERVER_URL + 'calendar');
+  }
 }
