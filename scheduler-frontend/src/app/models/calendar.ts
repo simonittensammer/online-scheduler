c504d7b1b853fa2d7ceac4fd9a2f376a875228af
simonittensammer <simon.ittensammer@gmx.at>
2021-01-06 01:32:00 +0100

    add attributes to calendar model

--- scheduler-frontend/src/app/models/calendar.ts
+++ scheduler-frontend/src/app/models/calendar.ts
@@ -1,6 +1,15 @@
+import {Appointment} from './appointment';
+
 export class Calendar {
   name: string;
   description: string;
   pw: string;
-  appointments: Array<>;
+  appointments: Array<Appointment>;
+
+  constructor(name: string, description: string, pw: string, appointments: Array<Appointment>) {
+    this.name = name;
+    this.description = description;
+    this.pw = pw;
+    this.appointments = appointments;
+  }
 }
