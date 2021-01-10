a265b05cb53873f89f8c6809eedb16cb383b47aa
simonittensammer <simon.ittensammer@gmx.at>
2021-01-10 19:45:33 +0100

    add date,start and end in angular model

--- scheduler-frontend/src/app/models/appointment.ts
+++ scheduler-frontend/src/app/models/appointment.ts
@@ -2,10 +2,16 @@ export class Appointment {
   id: number;
   title: string;
   description: string;
+  date: string;
+  start: string;
+  end: string;
 
-  constructor(id: number, title: string, description: string) {
+  constructor(id: number, title: string, description: string, date: string, start: string, end: string) {
     this.id = id;
     this.title = title;
     this.description = description;
+    this.date = date;
+    this.start = start;
+    this.end = end;
   }
 }
