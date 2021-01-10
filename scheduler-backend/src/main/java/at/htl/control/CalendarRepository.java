23f91faa183057684bfc8cc254488fe8c6b57a25
simonittensammer <simon.ittensammer@gmx.at>
2021-01-10 20:25:52 +0100

    sort appointments by date and time

--- scheduler-backend/src/main/java/at/htl/control/CalendarRepository.java
+++ scheduler-backend/src/main/java/at/htl/control/CalendarRepository.java
@@ -1,5 +1,6 @@
 package at.htl.control;
 
+import at.htl.entity.Appointment;
 import at.htl.entity.Calendar;
 import io.quarkus.hibernate.orm.panache.Panache;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
@@ -7,6 +8,9 @@ import org.hibernate.Hibernate;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @Transactional
 @ApplicationScoped
@@ -20,6 +24,16 @@ public class CalendarRepository implements PanacheRepository<Calendar> {
         Calendar calendar = find("name", name).firstResult();
 
         if (calendar != null) {
+            List<Appointment> appointments = calendar.getAppointments();
+
+            Comparator<Appointment> compareByStartTime = (Appointment o1, Appointment o2) -> o1.getStart().compareTo(o2.getStart());
+            Comparator<Appointment> compareByDate = (Appointment o1, Appointment o2) -> o1.getDate().compareTo(o2.getDate());
+
+            Collections.sort(appointments, compareByStartTime);
+            Collections.sort(appointments, compareByDate);
+
+            calendar.setAppointments(appointments);
+
             Hibernate.initialize(calendar);
             Hibernate.initialize(calendar.getAppointments());
         }
