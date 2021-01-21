3ca43b81d8ace16544c5859a08e6cfdae380e631
simonittensammer <simon.ittensammer@gmx.at>
2021-01-21 19:20:29 +0100

    add toggle Password-Input functionality

--- scheduler-frontend/src/app/pages/calendar-form/calendar-form.component.ts
+++ scheduler-frontend/src/app/pages/calendar-form/calendar-form.component.ts
@@ -14,6 +14,7 @@ import {MatSnackBar} from '@angular/material/snack-bar';
 export class CalendarFormComponent implements OnInit {
 
   calendarForm!: FormGroup;
+  hide: boolean = true;
 
   constructor(
     private router: Router,
